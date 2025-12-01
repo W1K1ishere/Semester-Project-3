6d12c294a7d1afeb30872009b7b7689b96a3c8d9
Netro <baheg24@student.sdu.dk>
2025-12-01 09:24:15 +0100

    Breaktimes from DB
    the code now accesses breaktimes from a database (did not create migrations yet) (time needs to be added as timestamp to database)

--- Tabely/app/Console/Commands/CheckTimeAndRunBreak.php
+++ Tabely/app/Console/Commands/CheckTimeAndRunBreak.php
@@ -13,38 +13,33 @@ class CheckTimeAndRunBreak extends Command
     protected $signature = 'wifi2ble:checkbreak';
     protected $description = 'Check the time and run the wifi2ble API request if it matches';
 
-    protected $scheduledTimes = [
-        '09:00',
-        '11:12',
-        '11:13',
-        '11:14',
-        '19:30',
-        '19:31',
-        '19:32',
-        '19:33',
-        '19:34',
-        '19:35',
-        '19:36',
-        '19:37',
-        '19:38',
-        '19:39',
-        '19:40',
-        '19:41',
-        '19:42',
-    ];
 
     public function handle()
 {
     $now = Carbon::now()->format('H:i');
 
+    $breakTimes = DB::table('breaks')->get();
+
     Log::info("⏱ Scheduler running, current time: {$now}");
 
-    foreach ($this->scheduledTimes as $time) {
-        if ($time === $now) {
+    foreach ($breakTimes as $break) {
+        
+        //this was to test formatting stuff
+        /*
+        if (true) {
+              SendWifi2BleRequestBreak::dispatch();
+              $willthiswork = date('H:i', $break->startTime);
+              
+            Log::info(" Time matched ({$now}) ({$willthiswork})— job dispatched");
+        }
+        */
+
+        if (date('H:i', $break->startTime) === $now) {
               SendWifi2BleRequestBreak::dispatch();
 
             Log::info(" Time matched ({$now}) — job dispatched");
         }
+        
     }
 }
 
